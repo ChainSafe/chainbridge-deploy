858b9e3363706c1dae95d9f4f541c9165cee4508
Gregory Markou <gregorymarkou@gmail.com>
2020-10-03 14:19:05 -0400

    use ethers conversion

--- cb-sol-cli/cmd/utils.js
+++ cb-sol-cli/cmd/utils.js
@@ -42,7 +42,7 @@ const waitForTx = async (provider, hash) => {
 }
 
 const expandDecimals = (amount, decimals = 18) => {
-    return ethers.utils.bigNumberify(String(amount).padStart(decimals, '0'));
+    return ethers.utils.parseUnits(String(amount), decimals);
 }
 
 const log = (args, msg) => console.log(`[${args.parent._name}/${args._name}] ${msg}`)
