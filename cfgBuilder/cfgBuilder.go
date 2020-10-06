f24f6b54ce9fb368522f103a88af1bd68e04e702
Stephanie <stephanie@chainsafe.io>
2020-10-06 08:34:37 -0400

    update references to gasPrice to maxGasPrice (#19)
    * update references to gasPrice to maxGasPrice to reflect changes in chainbridge opts
    
    * running linter
    
    * change MaxGasPrice to maxGasPrice
    
    * Commit David's style suggestion
    
    Co-authored-by: David Ansermino <david@chainsafe.io>
    
    Co-authored-by: David Ansermino <david@chainsafe.io>

--- cfgBuilder/cfgBuilder.go
+++ cfgBuilder/cfgBuilder.go
@@ -47,7 +47,7 @@ type Opts struct {
 	Erc721Handler  string `json:"erc721Handler,omitempty"`
 	GenericHandler string `json:"genericHandler,omitempty"`
 	GasLimit       string `json:"gasLimit,omitempty"`
-	GasPrice       string `json:"gasPrice,omitempty"`
+	MaxGasPrice    string `json:"maxGasPrice,omitempty"`
 	StartBlock     string `json:"startBlock"`
 	Http           string `json:"http,omitempty"`
 }
@@ -61,7 +61,7 @@ type EthChainConfig struct {
 	Erc721Handler  string   `json:"erc721Handler"`
 	GenericHandler string   `json:"genericHandler"`
 	GasLimit       string   `json:"gasLimit"`
-	GasPrice       string   `json:"gasPrice"`
+	MaxGasPrice    string   `json:"maxGasPrice"`
 	StartBlock     string   `json:"startBlock"`
 	Http           string   `json:"http"`
 	Relayers       []string `json:"relayers"`
@@ -115,7 +115,7 @@ func constructEthChainConfig(cfg EthChainConfig, relayer string) RawChainConfig 
 			Erc721Handler:  cfg.Erc721Handler,
 			GenericHandler: cfg.GenericHandler,
 			GasLimit:       cfg.GasLimit,
-			GasPrice:       cfg.GasPrice,
+			MaxGasPrice:    cfg.MaxGasPrice,
 			StartBlock:     cfg.StartBlock,
 			Http:           cfg.Http,
 		},
